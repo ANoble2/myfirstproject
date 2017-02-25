356d6ac4febf2571447df17141d498148fdc4215
ANoble2 <1407763@rgu.ac.uk>
2017-02-25 21:15:58 +0000

    superhero

--- phpDemo/superHero.php
+++ phpDemo/superHero.php
@@ -6,10 +6,10 @@
 
 <form action="superHeroResponse.php" method="post">
     <label>Forename</label><input type="text" name="forename">
-    <label>Surname</label><input type= "text" name="surname" >
+    <label>Surname</label><input type= "text" name="surname" ><br>
     <input type="radio" name="gender" value="male" checked > Male<br>
     <input type="radio" name="gender" value="female"> Female<br>
-   <label>Comment:</label> <textarea name="comment" rows="5" cols="40"></textarea>
+   Comment:<textarea name="comment" rows="5" cols="40"></textarea><br>
     <input type="submit" value="Submit">
 </form>
 
