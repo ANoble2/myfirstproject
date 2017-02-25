296afd373dfd56ebbfc0494e5a34acde64d6c591
ANoble2 <1407763@rgu.ac.uk>
2017-02-25 15:16:17 +0000

    login

---
+++ phpDemo/act1.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ashley
+ * Date: 25/02/2017
+ * Time: 15:09
+ */
+<?php
+    $myage = 18;
+    if ($myage > "16") {
+        print "buy specs!";
+    }
+    elseif ($myage > "18") {
+        print "buy mugs";
+    }
+    else { print "buy sausage rolls";}
+
+    ?>
--- phpDemo/index.php
+++ phpDemo/index.php
@@ -1,13 +1,35 @@
+<!DOCTYPE html> 
+<html>
+<head>
+</head>
+    <body>
+<p>
+    <?php
+    echo "Hello World";
+    echo "Hello,".""."world"."!";
+    echo 5*7;
+    $myname = "Frodo Baggins";
+    $myage = 111;
+    echo "My name is" . $myname . "and I am " . $myage;
+
+    ?>
+
+    <?php
+    $myage = "24";
+    if ($myage == "Simon") {
+        print "I know you!";
+    }
+    else {
+        print "Who are you?";
+    }
+    ?>
+</p>
+</body>
+
+</html>
 <?php
-/**
- * Created by PhpStorm.
- * User: 1407763
- * Date: 14/02/2017
- * Time: 16:35
- */
-
-echo "Its alive!!!";
-
-phpinfo();
-
+echo "I get printed!";
+// I don't! I'm a comment.
+/* I don’t get printed either
+and neither do I */
 ?>
