662225f0433162da40c10b9e923c397037702e2b
ANoble2 <1407763@rgu.ac.uk>
2017-02-25 16:22:34 +0000

    login

--- phpDemo/act3.php
+++ phpDemo/act3.php
@@ -5,21 +5,20 @@
 </head>
 <body>
 <?php
-/**
- * Created by PhpStorm.
- * User: Ashley
- * Date: 25/02/2017
- * Time: 16:03
- */
+
 
 $provisionedActivities = array("Specs", "Mugs", "Sausage Rolls"); // declares the array
 
 echo "<h1>My Array</h1>";
 printprovisionedActivities($provisionedActivities);
-$provisionedActivities[1] = "Mugs"; // modifies position 1 (re)
-echo "<h1>Swap in Mugs</h1>";
+
+$provisionedActivities[1] = "Hats"; // modifies position 1 (re)
+
+echo "<h1>Swap in Hats</h1>";
 printprovisionedActivities($provisionedActivities);
+
 unset($provisionedActivities[2]); // removes the array in position 2
+
 echo "<h1>Take out Sausage Rolls</h1>";
 printprovisionedActivities($provisionedActivities);
 
@@ -37,3 +36,21 @@ function printprovisionedActivities ($provisionedActivities)
 ?>
 </body>
 </html>
+<?php
+$myArray = array("specs", "mugs", "sausage rolls");
+echo "<h1>Original Array</h1>";
+printmyArray($myArray);
+$myArray[1] = "hugs"; // modifies position 1 (re)
+echo "<h1>Swap in Hugs</h1>";
+printmyArray($myArray);
+unset($myArray[2]); // removes the array in position 2
+echo "<h1>Take out Sausage Rolls</h1>";
+printmyArray($myArray);
+//I've made a function to print out the array instead of writing the same code repeatedly above.
+function printmyArray($myArray)
+{
+foreach($myArray as $x) {
+echo "<p>" . $x . "</p>";
+}
+}
+?>
