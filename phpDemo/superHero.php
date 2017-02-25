f5cd71b35dffb8afb399bcc0663ba4cd9e666596
ANoble2 <1407763@rgu.ac.uk>
2017-02-25 21:41:40 +0000

    superhero

--- phpDemo/superHero.php
+++ phpDemo/superHero.php
@@ -3,7 +3,16 @@
 <head>
 </head>
 <body>
+<?php
+if(isset($_POST['submit'])) {
+    $forename = $_POST["forename"];
+    $surname = $_POST["surname"];
+    $gender = $_POST["gender"];
+    $comment = $_POST["comment"];
 
+    echo $forename . $surname . $gender . $comment;
+}
+?>
 <form action="" method="post" <?php echo $_SERVER['PHP_SELF']; ?>>
     <label>Forename</label><input type="text" name="forename">
     <label>Surname</label><input type= "text" name="surname" ><br>
@@ -11,16 +20,6 @@
     <input type="radio" name="gender" value="female"> Female<br>
    Comment:<textarea name="comment" rows="5" cols="40"></textarea><br>
     <input type="submit" value="Submit">
-    <?php
-    if(isset($_SERVER['submit'])) {
-        $forename = $_POST["forename"];
-        $surname = $_POST["surname"];
-        $gender = $_POST["gender"];
-        $comment = $_POST["comment"];
-
-        echo $forename . $surname . $gender . $comment;
-    }
-    ?>
 
 
 </form>
