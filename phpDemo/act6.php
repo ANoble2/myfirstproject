d2a04ad47ac6d5aed082fb612f36c86452c5ed8c
ANoble2 <1407763@rgu.ac.uk>
2017-02-25 17:11:54 +0000

    lottery

---
+++ phpDemo/act6.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html> 
+<html>
+<head>
+</head>
+<body>
+<?php
+
+//Make an array
+$lotteryArray = array("Ashley", "Petr", "Joesph", "Brian", "William", "Steve", "Mike", "Fiona" , "Heather", "Mary");
+//Sort the array
+sort($lotteryArray);
+//Select the winner
+$winner = (rand(0,count($lotteryArray))) - 1;
+//Prints the winner
+echo "<p>The winner of all the specs is " . strtoupper($lotteryArray[$winner]) . "</p>";
+//Remove the person from the array
+unset($array[$winner]);
+//Do the same again for mugs
+$winner = (rand(0,count($lotteryArray))) - 1;
+echo "<p>The winner of all the mugs is " . strtoupper($lotteryArray[$winner]) . "</p>";
+unset($array[$winner]);
+//and the sausuage rolls
+$winner = (rand(0,count($lotteryArray))) - 1;
+echo "<p>The winner of all the sausage rolls is " . strtoupper($lotteryArray[$winner]) . "</p>";
+
+
+?>
+
+</body>
+</html>
