8920df63be8f105d99b8acdd5a90570965fad441
ANoble2 <1407763@rgu.ac.uk>
2017-02-25 16:17:21 +0000

    login

--- phpDemo/act3.php
+++ phpDemo/act3.php
@@ -13,7 +13,21 @@
  */
 
 $provisionedActivities = array("Specs", "Mugs", "Sausage Rolls"); // declares the array
-function printmyArray($provisionedActivities)
+
+echo "<h1>My Array</h1>";
+printprovisionedActivities($provisionedActivities);
+$provisionedActivities[1] = "Mugs"; // modifies position 1 (re)
+echo "<h1>Swap in Mugs</h1>";
+printprovisionedActivities($provisionedActivities);
+unset($provisionedActivities[2]); // removes the array in position 2
+echo "<h1>Take out Sausage Rolls</h1>";
+printprovisionedActivities($provisionedActivities);
+
+
+
+
+
+function printprovisionedActivities ($provisionedActivities)
 {
     foreach($provisionedActivities as $x) {
         echo "<p>" . $x . "</p>";
