5a5c13ed1f146920a0c29770f3c5710576b00206
ANoble2 <1407763@rgu.ac.uk>
2017-02-25 17:23:48 +0000

    wanted

---
+++ phpDemo/act7.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html> 
+<html>
+<head>
+</head>
+<body>
+<?php
+
+function awardforcapture($specsowned, $mugsowned, $sausagerollsowned)
+{
+    $award = 10 * (($specsowned * $mugsowned * $sausagerollsowned)/2);
+    return $award;
+}
+function printWantedBanner($name, $specsowned, $mugsowned, $sausagerollsowned)
+{
+    echo "<p><strong>Wanted:</strong> ". $name . "</p>";
+    echo "<p>Known to be in posession of the following items:</p>";
+    echo "<p>Specs: " . $specsowned . "</p>";
+    echo "<p>Mugs:" . $mugsowned . "</p>";
+    echo "<p>Sausage Rolls: " . $sausagerollsowned . "</p>";
+    echo "<p>Award for capture: £" . awardforcapture($specsowned,$mugsowned,$sausagerollsowned) . "</p><br><br>";
+}
+printWantedBanner("Ashley",1,2,3);
+printWantedBanner("Rusty",3,4,5);
+?>
+
+
+</body>
+</html>
