034124059493adcc0de9309953fbe97c493ab23a
ANoble2 <1407763@rgu.ac.uk>
2017-02-25 15:17:46 +0000

    login

--- phpDemo/act1.php
+++ phpDemo/act1.php
@@ -7,10 +7,10 @@
  */
 <?php
     $myage = 18;
-    if ($myage > "16") {
+    if ($myage > 16) {
         print "buy specs!";
     }
-    elseif ($myage > "18") {
+    elseif ($myage > 18) {
         print "buy mugs";
     }
     else { print "buy sausage rolls";}
