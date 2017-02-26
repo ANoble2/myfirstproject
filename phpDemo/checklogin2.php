9c1d99399a443d3b62909810acc93b1a25d0def5
ANoble2 <1407763@rgu.ac.uk>
2017-02-26 12:37:30 +0000

    login test activity2

---
+++ phpDemo/checklogin2.php
@@ -0,0 +1,13 @@
+<?php
+$username = $_POST["name"];
+$password = $_POST["password"];
+
+if ($username =="Ashley" && $password=="cisco")
+{
+    session_start();
+    $_SESSION('access_level_session','standarduser');
+}
+
+header('Location: homepage2.php');
+
+?>
