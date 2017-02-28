0db20ec3844fe33e504425c319cd8a00f79c7a3b
1407763 <1407763@rgu.ac.uk>
2017-02-28 13:11:16 +0000

    super hero change

--- phpDemo/superHero.php
+++ phpDemo/superHero.php
@@ -15,10 +15,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 <form action="" method="post">
     <label>Forename</label><input type="text" name="forename">
-    <label>Surname</label><input type= "text" name="surname" ><br>
+    <label>Surname</label><input type= "text" name="surname" ><br><br>
     <input type="radio" name="gender" value="male" checked > Male<br>
     <input type="radio" name="gender" value="female"> Female<br>
-   Super Power choice:<textarea name="comment" rows="5" cols="40"></textarea><br>
+   Super Power:<textarea name="comment" rows="5" cols="40"></textarea><br>
     <input type="submit" value="Submit">
 </form>
 
