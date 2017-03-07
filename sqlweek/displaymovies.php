280111d7d6a3ffff698e06347dc239d77f604a1d
1407763 <1407763@rgu.ac.uk>
2017-03-07 15:14:36 +0000

    teacher changed

--- sqlweek/displaymovies.php
+++ sqlweek/displaymovies.php
@@ -1,15 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 1407763
- * Date: 07/03/2017
- * Time: 13:12
- */
-
-
-
-
-
+include ("dbConnect.php");
 ?>
 
 <!DOCTYPE html> 
@@ -17,8 +7,31 @@
 <head>
 </head>
 <body>
+<header>
+    <h1>My First Database Thing!</h1>
+</header>
 
+<body>
 
+<?php
+// create a SQL query as a string
+$sql_query = "SELECT * FROM marvelmovies";
+// execute the SQL query
+$result = $link->query($sql_query);
+// iterate over $result object one $row at a time
+// use fetch_array() to return an associative array
+while($row = $result->fetch_array()){
+$movieTitle = $row['title'];
+    echo "<p>{$movieTitle}</p>";
+}
+
+?>
+
+</body>
+
+<footer>
+
+</footer>
 
 </body>
 </html>
