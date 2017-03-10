72d4739aadb1c4a65d3fdc63ed94a44c6ea1bc97
ANoble2 <1407763@rgu.ac.uk>
2017-03-10 16:33:18 +0000

    web app

--- Coursework/index.php
+++ Coursework/index.php
@@ -82,45 +82,51 @@ if(isset($_POST['btn-login'])){
 </head>
 <body>
 
-<div class="container">
-    <div class="jumbotron">
-        <h1>Visual Upload</h1>
-        <img src="material/images/image%20gallery.PNG" class="img-rounded" alt="logoimage" width="171" height="180">
-    <div style="width: 500px; margin: 50px auto;">
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
-            <center><h2>Login</h2></center>
-            <hr/>
-            <?php
-            if(isset($errorMsg)){
-                ?>
-                <div class="alert alert-danger">
-                    <span class="glyphicon glyphicon-info-sign"></span>
-                    <?php echo $errorMsg; ?>
-                </div>
+
+<div class="jumbotron">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h1> Viusal upload</h1><br>
+                <img src="material/images/logo.PNG" width="304" height="236"/>
+            </div>
+            <div class="col-md-5
+            <div style="width: 500px; margin: 50px auto;" >
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
+                <center><h2>Login</h2></center>
+                <hr/>
                 <?php
-            }
-            ?>
-            <div class="form-group">
-                <label for="email" class="control-label">Email</label>
-                <input type="email" name="email" class="form-control" autocomplete="off">
-                <span class="text-danger"><?php if(isset($errorEmail)) echo $errorEmail; ?></span>
-            </div>
-            <div class="form-group">
-                <label for="password" class="control-label">Password</label>
-                <input type="password" name="password" class="form-control" autocomplete="off">
-                <span class="text-danger"><?php if(isset($errorPassword)) echo $errorPassword; ?></span>
-            </div>
-            <div class="form-group">
-                <center><input type="submit" name="btn-login" value="Login" class="btn btn-primary"></center>
-            </div>
-            <hr/>
-            <a href="register.php">Register</a>
-        </form>
+                if(isset($errorMsg)){
+                    ?>
+                    <div class="alert alert-danger">
+                        <span class="glyphicon glyphicon-info-sign"></span>
+                        <?php echo $errorMsg; ?>
+                    </div>
+                    <?php
+                }
+                ?>
+                <div class="form-group">
+                    <label for="email" class="control-label">Email</label>
+                    <input type="email" name="email" class="form-control" autocomplete="off">
+                    <span class="text-danger"><?php if(isset($errorEmail)) echo $errorEmail; ?></span>
+                </div>
+                <div class="form-group">
+                    <label for="password" class="control-label">Password</label>
+                    <input type="password" name="password" class="form-control" autocomplete="off">
+                    <span class="text-danger"><?php if(isset($errorPassword)) echo $errorPassword; ?></span>
+                </div>
+                <div class="form-group">
+                    <center><input type="submit" name="btn-login" value="Login" class="btn btn-primary"></center>
+                </div>
+                <hr/>
+                <a href="register.php">Register</a>
+            </form>
+        </div>
     </div>
 </div>
 </div>
 
-<img src="material/images/responsive.PNG" class="img-rounded" alt="Responsive" width="304" height="236">
+
 
 
 
