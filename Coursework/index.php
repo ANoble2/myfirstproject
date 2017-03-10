57a05c99b4b1b6559ef784d60093d7d1505c3c29
ANoble2 <1407763@rgu.ac.uk>
2017-03-10 15:41:00 +0000

    web app

--- Coursework/index.php
+++ Coursework/index.php
@@ -85,7 +85,7 @@ if(isset($_POST['btn-login'])){
 <div class="container">
     <div class="jumbotron">
         <h1>Hi</h1>
-
+        <img src="material/images/logoimage.PNG" class="img-rounded" alt="Responsive" width="304" height="236">
     <div style="width: 500px; margin: 50px auto;">
         <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
             <center><h2>Login</h2></center>
