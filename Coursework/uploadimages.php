0b827c41b6007a8afde13034af89adffd8c5c68d
ANoble2 <1407763@rgu.ac.uk>
2017-03-11 23:16:50 +0000

    images test 2

--- Coursework/uploadimages.php
+++ Coursework/uploadimages.php
@@ -7,7 +7,7 @@
  */
 session_start();
 include ('dbConnect.php');
-$upload_dir = '/Uploads/';
+$upload_dir = "uploads/";
 
 // if upload button is pressed passes variables entered in form
 if(isset($_POST['btnUpload'])) {
