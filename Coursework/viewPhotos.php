3079b289c721a115918ab495274429b109a973da
1407763 <1407763@rgu.ac.uk>
2017-03-15 12:34:40 +0000

    login system

--- Coursework/viewPhotos.php
+++ Coursework/viewPhotos.php
@@ -9,9 +9,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
+session_start();
 include ('dbConnect.php');
 $upload_dir = 'uploads/';
+
 if(isset($_GET['delete'])){
    $id = $_GET['delete'];
 
