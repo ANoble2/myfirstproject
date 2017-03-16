b879973fe77672f7bc47f41fd20244e6f3780637
ANoble2 <1407763@rgu.ac.uk>
2017-03-16 19:39:49 +0000

    Coursework

--- Coursework/gallery.php
+++ Coursework/gallery.php
@@ -86,7 +86,7 @@ $upload_dir = 'uploads/'; // specifies the directory where the file is going to 
             <div class="col-md-3">
                 <div class="row">
 
-                    <center><img src="<?php echo $upload_dir . $row['image'] ?>" data-lity class="img-responsive" width="300" max-height:220"></center> <!-- display image from database -->
+                    <center><img src="<?php echo $upload_dir . $row['image'] ?>" data-lity class="img-responsive"></center> <!-- display image from database -->
                    <center><p><?php echo $row['name'] ?></p></center> <!-- display image name along with image -->
             </div> <!-- end of row div -->
         </div>  <!-- end of col-md-4 div -->
