958fd45e403c2fe29a5e2e8656aa23e70db7fe61
ANoble2 <1407763@rgu.ac.uk>
2017-03-16 16:41:18 +0000

    Coursework

--- Coursework/gallery.php
+++ Coursework/gallery.php
@@ -98,7 +98,33 @@ $upload_dir = 'uploads/'; // specifies the directory where the file is going to 
             }
             }
             ?>
+            <div class="table-responsive table-bordered">
+                <table class=" table table-bordered table-responsive">
 
+                    <tbody>
+
+                    <?php
+                    $sql = "select * from tbl_images"; // select all from table tbl_images
+                    $result = mysqli_query($link, $sql);
+                    if(mysqli_num_rows($result)){
+                        while($row = mysqli_fetch_assoc($result)) {
+
+                            ?>
+
+                            <tr>
+
+                                <td><?php echo $row['name'] ?></td>
+
+                                <td><img src="<?php echo $upload_dir.$row['image'] ?>" height="40"> </td> <!-- display image from database -->
+
+                            </tr>
+                            <?php
+                        }
+                    }
+                    ?>
+                    </tbody> <!-- end of table body -->
+                </table> <!-- end of table -->
+            </div> <!-- end of table div -->
             <div class="container"
         <hr> <!-- creates line to break up content -->
         <footer class="footer"> <!-- start of footer for page  -->
@@ -110,49 +136,9 @@ $upload_dir = 'uploads/'; // specifies the directory where the file is going to 
 
 </div> <!-- end of gallery div -->
 
-<?php
-$sql = "select * from tbl_images"; // select all from table tbl_images
-$result = mysqli_query($link, $sql);
-if(mysqli_num_rows($result)){
-while($row = mysqli_fetch_assoc($result)) {
 
-?>
 
-<?php
-$img = $item['image'];
-$rows = $result->num_rows;    // Find total rows returned by database
-if($rows > 0) {
-    $cols = 4;    // Define number of columns
-    $counter = 1;     // Counter used to identify if we need to start or end a row
-    $nbsp = $cols - ($rows % $cols);    // Calculate the number of blank columns
 
-    $container_class = 'container-fluid';  // Parent container class name
-    $row_class = 'row';    // Row class name
-    $col_class = 'col-md-4'; // Column class name
-
-    echo '<div class="'.$container_class.'">';    // Container open
-    while ($item = $result->fetch_array()) {
-        if(($counter % $cols) == 1) {    // Check if it's new row
-            echo '<div class="'.$row_class.'">';	// Start a new row
-        }
-
-        $img = $item['image'];
-        echo '<div class="'.$col_class.'">'.$img.'<h5>'.$item['name'].'</h5></div>';     // Column with content
-        if(($counter % $cols) == 0) { // If it's last column in each row then counter remainder will be zero
-            echo '</div>';	 //  Close the row
-        }
-        $counter++;    // Increase the counter
-    }
-    $result->free();
-    if($nbsp > 0) { // Adjustment to add unused column in last row if they exist
-        for ($i = 0; $i < $nbsp; $i++)	{
-            echo '<div class="'.$col_class.'">&nbsp;</div>';
-        }
-        echo '</div>';  // Close the row
-    }
-    echo '</div>';  // Close the container
-}
-?>
 
 <!-- scripts -->
 <script src="material/js/bootstrap.min.js"></script>
