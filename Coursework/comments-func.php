590aba3af39b90adea49699b0ca8768a59f03b8c
ANoble2 <1407763@rgu.ac.uk>
2017-03-17 19:54:13 +0000

    Coursework

--- Coursework/comments-func.php
+++ Coursework/comments-func.php
@@ -23,8 +23,26 @@ function retrieveComments($link) { // to retrieve comments from the database, li
     $sql = "select * from tbl_comments"; // query the database
     $result = $link->query($sql); // variable to store connection to use query on sql variable about with select statement above
     while ( $row = $result->fetch_assoc()) { // loop through all messages to display all until none left
-        echo $row['message']; // specify what you want to be displayed on page
+        echo "<div class='boxCom'><p>";
+            echo $row['uid']."<br>"; // display user who posted comment
+            echo $row['date']."<br>"; // display date of when comment posted
+            echo nl2br($row['message']); // specify what you want to be displayed on page, nl2br to create line breaks in messages
+        echo "</p>
+            <form class='delete-form' method='post' action='".deletePosts($link)."'>
+            <input type ='hidden' name='cid' value='".$row['cid']."'>
+            <button type='submit' name='deletePost'>Delete</button>
+            
+</form>
+</div>";
     }
 }
 
+function deletePosts($link) {
+    if (isset($_POST['deletePost'])) { // unless button is pressed shouldn't run code below
+        $cid = $_POST['cid'];
+        $sql = "delete from tbl_comments where cid='$cid'";
+        $result = $link->query($sql); // variable to store connection to use query on sql variable about with update statement above
+        header("location: comments.php");
+    }
+}
 ?>
--- Coursework/comments.php
+++ Coursework/comments.php
@@ -33,7 +33,7 @@ echo "<form method='POST' action='".insComments($link)."'>
     <input type='hidden' name='uid' value='anonymous'>
     <input type='hidden' name='date' value='".date('Y-m-d H:i:s')."'>
     <textarea name='message'></textarea><br>
-    <button type='submit' name='submitComment'>Comment</button>
+    <button type='submit' name='submitComment'>Post Comment</button>
 </form>";
 
 retrieveComments($link);
