6713d93013ae8bbde53e240773a220d70be2fd3b
ANoble2 <1407763@rgu.ac.uk>
2017-03-18 15:09:14 +0000

    test css

--- Coursework/index.php
+++ Coursework/index.php
@@ -64,6 +64,7 @@ if(isset($_POST['btn-login'])){ // if btn-login is pressed
 
     <!-- Bootstrap -->
     <link href="material/css/bootstrap.css" rel="stylesheet">
+    <script src="material/js/bootstrap.min.js"></script>
 
     <nav class="navbar navbar-default"> <!-- start of nav bar -->
         <div class="container"> <!-- start of container -->
