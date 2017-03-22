237b5af0f9b46c0e79506219cba0202a50ab4b23
1407763 <1407763@rgu.ac.uk>
2017-03-22 12:11:44 +0000

    users to photos

--- Coursework/comments.php
+++ Coursework/comments.php
@@ -21,13 +21,57 @@ $upload_dir = 'uploads/'; // specifies the directory where the file is going to 
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Ashley's Project</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <title>Ashley's Project</title> <!-- title page -->
 
     <!-- Bootstrap -->
     <link href="material/css/bootstrap.css" rel="stylesheet">
+    <link href="material/css/bootstrap-theme.min.css" rel="stylesheet">
+
+
+    <nav class="navbar navbar-default"> <!-- start of nav bar -->
+        <div class="container"> <!-- start of container -->
+
+            <div class="navbar-header"> <!-- start of nav bar header div -->
+
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+
+                    <span class="icon-bar"></span> <!-- three lines appear on collapse button -->
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <a class="navbar-brand" href="#">Visual Upload</a> <!-- name of web app in nav bar section -->
+
+            </div> <!-- start of nav bar header div -->
+
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav"> <!-- section that holds links to other pages-->
+                    <li class=><a href="Home.php">Home</a></li>
+                    <li class=""><a href="gallery.php">Image Gallery</a></li>
+                    <li class="active"><a href="uploadimages.php">Upload Images</a></li>
+                    <li class=""><a href="viewPhotos.php">Control Panel</a></li>
+                </ul>
+                <form class="navbar-form navbar-right"> <!-- makes contents appear on the right -->
+                    Welcome : <?php echo $_SESSION['username']; ?> <!-- display logged in users name -->
+                    <a href="logout.php" class="btn btn-default" role="button"><span class="glyphicon glyphicon-log-out"></span> Log Out </a> <!-- log out button -->
+                </form> <!-- end of form -->
+
+            </div>  <!-- end of nav bar collapse div -->
+
+        </div> <!-- end of container -->
+
+    </nav><!--end of nav bar -->
+
+</head>
+
 
 
 </head>
