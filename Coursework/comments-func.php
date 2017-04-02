84a443587177556a16d61fc490635cef9d3727e8
ANoble2 <1407763@rgu.ac.uk>
2017-04-02 17:11:16 +0100

    message box

--- Coursework/comments-func.php
+++ Coursework/comments-func.php
@@ -33,6 +33,7 @@ function retrieveComments($link) { // to retrieve comments from the database, li
             <input type ='hidden' name='cid' value='".$row['cid']."'>
             <button type='submit' name='deletePost' class='btn btn-danger'><span class='glyphicon glyphicon-remove-sign'></span> Delete</button>
             <br>
+            <br>
             
 </form>
 </div>";
