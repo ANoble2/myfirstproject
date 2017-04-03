a2d021ef5bfac0cc99e74341ae9a7cfdaea0dc55
ANoble2 <1407763@rgu.ac.uk>
2017-04-03 20:05:38 +0100

    testing

---
+++ Coursework/password test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ashley
+ * Date: 03/04/2017
+ * Time: 19:40
+ */
+//simpletut password hashing
+//https://www.youtube.com/watch?v=RCr0Go3Z0u8
+//register part
+//$StorePassword = password_hash($password,PASSWORD_BCRYPT,array('cost' => 10));
+//change $paasword variable in the insert query to $store password
+
+//login page part
+
+//if (password_verify($password,$row['password'] )){
+
+//$password = md5($password); //encrypt password with md5
+//$password = md5($password);
+?>
