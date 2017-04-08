cef6e64f4ab6eb5c12d87652b4113cbb68984250
ANoble2 <1407763@rgu.ac.uk>
2017-04-08 13:53:56 +0100

    testing

--- Coursework/gallery.php
+++ Coursework/gallery.php
@@ -136,7 +136,7 @@ $target_dir = 'uploads/'; // specifies the directory where the file is going to 
 
             <div class="col-md-3">
                 <ul class="row">
-                    <center> <a></a> <img src="<?php echo $target_dir. $row['image'] ?>"  height="168" width="290" data-lity class="img-thumbnail"></a></center> <!-- display image from database -->
+                    <center> <img src="<?php echo $target_dir. $row['image'] ?>"  height="168" width="290" data-lity class="img-responsive"></center> <!-- display image from database -->
                     <center> <p><?php echo $row['name'] ?></p></center> <!-- display image name along with image -->
                     <center> <p>Uploaded by <?php echo $row ['username']?></p></center>
                     <center> <a class="btn btn-primary btn-sm" href="comments.php"><span class="glyphicon glyphicon-comment"></span> Post or View Comments</a></center>
