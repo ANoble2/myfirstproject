a1df77ea9867c12acf3cd2bb5f3d34de852c3612
ANoble2 <1407763@rgu.ac.uk>
2017-04-08 19:04:58 +0100

    testing

--- Coursework/gallery.php
+++ Coursework/gallery.php
@@ -139,20 +139,32 @@ $target_dir = 'uploads/'; // specifies the directory where the file is going to 
         while ($row = mysqli_fetch_assoc($result)) {
             ?>
 
-
-
-            <div class="col-md-3">
-                <ul class="row">
-                    <center> <img src="<?php echo $target_dir. $row['image'] ?>"  height="168" width="290" data-lity class="img-responsive"></center> <!-- display image from database -->
-                    <br>
-                    <center> <p><?php echo $row['name'] ?></p></center> <!-- display image name along with image -->
-                    <center> <p>Uploaded by <?php echo $row ['username']?></p></center>
-                    <center> <a class="btn btn-primary btn-sm" href="comments.php"><span class="glyphicon glyphicon-comment"></span> Post or View Comments</a></center>
-                    <br>
-                </ul>
-            </div>  <!-- end of col div -->
-
             <?php
+            $sql = "select * from tbl_users"; // select all from table tbl_images
+            $result = mysqli_query($link, $sql);
+            if (mysqli_num_rows($result)) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    ?>
+
+                    <div class="col-md-3">
+                        <ul class="row">
+                            <center><img src="<?php echo $target_dir . $row['image'] ?>" height="168" width="290"
+                                         data-lity
+                                         class="img-responsive"></center> <!-- display image from database -->
+                            <br>
+                            <center><p><?php echo $row['name'] ?></p></center>
+                            <!-- display image name along with image -->
+                            <center><p>Uploaded by <?php echo $row ['username'] ?></p></center>
+                            <center><a class="btn btn-primary btn-sm" href="comments.php"><span
+                                            class="glyphicon glyphicon-comment"></span> Post or View Comments</a>
+                            </center>
+                            <br>
+                        </ul>
+                    </div>  <!-- end of col div -->
+
+                    <?php
+                }
+            }
         }
     }
     ?>
