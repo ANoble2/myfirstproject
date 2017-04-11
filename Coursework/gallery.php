5b5bae1e99315a7bd5ceac361982c82cdc62ab64
1407763 <1407763@rgu.ac.uk>
2017-04-11 15:29:49 +0100

    users to photos

--- Coursework/gallery.php
+++ Coursework/gallery.php
@@ -146,7 +146,7 @@ $target_dir = 'uploads/'; // specifies the directory where the file is going to 
                     <center> <img src="<?php echo $target_dir. $row['image'] ?>"  height="168" width="290" data-lity class="img-responsive"></center> <!-- display image from database -->
                     <center> <p><?php echo $row['name'] ?></p></center> <!-- display image name along with image -->
                     <center> <p>Uploaded by <?php echo $row ['username']?></p></center>
-                    <center> <a class="btn btn-primary btn-sm" href="comments.php"><span class="glyphicon glyphicon-comment"></span> Post or View Comments</a></center>
+                    <center> <a class="btn btn-primary btn-sm" href="comments.php?id=<?php echo $row['id'] ?>"><span class="glyphicon glyphicon-comment"></span> Post or View Comments</a></center>
                     <br>
                 </ul>
             </div>  <!-- end of col div -->
