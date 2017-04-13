749de56f68eeed895c8ef142a5e66e2611ba59b1
ANoble2 <1407763@rgu.ac.uk>
2017-04-13 17:10:21 +0100

    comment box extras

--- Coursework/comments-func.php
+++ Coursework/comments-func.php
@@ -28,7 +28,7 @@ function insComments($link){ // insert comments to the database, link is connect
         $message = htmlspecialchars($message);
 
         if(empty($message)){
-            echo "</p> <span class='text-danger'>You didn't enter a Comment</span>";
+            echo "</p> <div class=\"alert alert-danger\" role=\"alert\">You Didn't Enter a Comment</div>";
         }else
        $sql = "insert into tbl_comments(uid, date, message,picture_id) 
                 VALUES ('$uid', '$date', '$message','$pic_id')"; // insert comment information into the tbl_comments table
