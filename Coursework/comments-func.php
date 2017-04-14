768476973c1cf3fc55b33a0bb1f770647f29ef75
1407763 <1407763@rgu.ac.uk>
2017-04-14 10:03:22 +0100

    users to photos

--- Coursework/comments-func.php
+++ Coursework/comments-func.php
@@ -43,8 +43,11 @@ function retrieveComments($link) { // to retrieve comments from the database, li
     "<input type='hidden' class='form-control ' name='pic_id' value='".$_GET['id']."'>";
     $pic_id = trim($_POST['pic_id']);
     $sql = "select * from tbl_comments where picture_id=$pic_id ORDER BY date DESC "; // query the database
-    $result = $link->query($sql); // variable to store connection to use query on sql variable about with select statement above
-    while ( $row = $result->fetch_assoc()) { // loop through all messages to display all until none left
+    $result = mysqli_query($link, $sql);
+    if(mysqli_num_rows($result) > 0){
+    $row = mysqli_fetch_assoc($result);
+   // $result = $link->query($sql); // variable to store connection to use query on sql variable about with select statement above
+   // while ( $row = $result->fetch_assoc()) { // loop through all messages to display all until none left
         echo "<div class='panel-primary'><p>";
             echo $row['uid']."<br>"; // display user who posted comment
             echo $row['date']."<br>"; // display date of when comment posted
