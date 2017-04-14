2934631b692295065de7fdd4894f0681d4f954fc
ANoble2 <1407763@rgu.ac.uk>
2017-04-14 13:25:24 +0100

    comment box extras

--- Coursework/comments-func.php
+++ Coursework/comments-func.php
@@ -42,7 +42,6 @@ function insComments($link){ // insert comments to the database, link is connect
 function retrieveComments($link) { // to retrieve comments from the database, link is connection
     $pic_id = trim($_POST['pic_id']);
     "input type='hidden' class='form-control ' name='pic_id' value='".$_POST['id']."'";
-    $pic_id = trim($_POST['pic_id']);
     $sql = "select * from tbl_comments join tbl_images ON picture_id=id WHERE picture_id=$pic_id ORDER BY date DESC"; // query the database
     $result = $link->query($sql); // variable to store connection to use query on sql variable about with select statement above
     while ( $row = $result->fetch_assoc()) { // loop through all messages to display all until none left
