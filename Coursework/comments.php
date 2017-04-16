f73409c696895188153c39111ee64979476916f5
ANoble2 <1407763@rgu.ac.uk>
2017-04-16 12:11:13 +0100

    comment box extras

--- Coursework/comments.php
+++ Coursework/comments.php
@@ -20,68 +20,13 @@ ini_set('session.cookie_httponly', true); // help against session hijacking with
 session_start(); // Start the session
 date_default_timezone_set('Europe/London');// takes current time specified when submit post
 include ('dbConnect.php'); // create connection to the database
-
+include 'comments-func.php'; // reference function for form to use
 if(!isset($_SESSION['username'])){ // check user logged in or not , if not redirect to login page (index.php)
     header('location:index.php');
 }
 $target_dir = 'uploads/'; // specifies the directory where the file is going to be placed
-
-function insComments($link){ // insert comments to the database, link is connection
-if (isset($_POST['submitComment'])) { // unless button is pressed shouldn't run code below
-$uid = $_POST['uid'];
-$date = $_POST['date'];
-$message = trim($_POST['message']);
-$pic_id = trim($_POST['pic_id']);
-
-//sanitize message comment from post
-$message = stripslashes($message);
-$message = mysqli_real_escape_string($link, $_POST ['message']);
-$message = htmlspecialchars(strip_tags($message));
-
-//display alert messages for when user posts a comment or doesn't
-if(empty($message)){
-echo "</p> <div class='alert alert-danger' role='alert'>Error You Need To Enter a Comment</div>";
-}else { echo "</p> <div class='alert alert-success' role='alert'>Successfully Posted Comment</div>";
-
-$sql = "insert into tbl_comments(uid, date, message,picture_id)
-VALUES ('$uid', '$date', '$message','$pic_id')"; // insert comment information into the tbl_comments table
-$result = $link->query($sql); // variable to store connection to use query on sql variable about with insert statement above
-}
-}
-}
-
-function retrieveComments($link) { // to retrieve comments from the database, link is connection
-    "<input type='hidden' class='form-control 'name='pic_id' value='".$_GET['id']."'>";
-    $pic_id = trim($_GET['pic_id']);
-$sql = "select * from tbl_comments where picture_id=$pic_id ORDER BY date DESC "; // query the database
-$result = $link->query($sql); // variable to store connection to use query on sql variable about with select statement above
-while ( $row = $result->fetch()) { // loop through all messages to display all until none left
-echo "<div class='panel-primary'><p>";
-        echo $row['uid']."<br>"; // display user who posted comment
-        echo $row['date']."<br>"; // display date of when comment posted
-        echo nl2br($row['message']); // specify what you want to be displayed on page, nl2br to create line breaks in messages
-        echo "</p>
-    <form class='form-group' method='post' action='".deletePosts($link)."'>
-        <input type ='hidden' name='cid' value='".$row['cid']."'>
-        <button type='submit' name='deletePost' class='btn btn-danger'><span class='glyphicon glyphicon-remove-sign'></span> Delete</button>
-        <br>
-        <br>
-
-    </form>
-</div>";
-}
-}
-
-function deletePosts($link) {
-if (isset($_POST['deletePost'])) { // unless delete button is pressed shouldn't run code below
-$cid = $_POST['cid'];
-
-$sql = "delete from tbl_comments where cid='$cid' and uid= '". $_SESSION['username'] ."'";
-$result = $link->query($sql); // variable to store connection to use query on sql variable about with update statement above
-header("location:comments.php");
-}
-}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -140,7 +85,7 @@ header("location:comments.php");
     <div class="jumbotron">
         <!-- Form code for users to post comments and back button to return to gallery -->
         <?php
-        echo "<form method='POST' action='comments.php".insComments($link)."'>
+        echo "<form method='POST' action='".insComments($link)."'>
     <input type='hidden' class='form-control 'name='uid' value='". $_SESSION['username'] ."'>
     <input type='hidden' class='form-control ' name='date' value='".date('Y-m-d H:i:s')."'>
     <input type='hidden' class='form-control ' name='pic_id' value='".$_GET['id']."'>
