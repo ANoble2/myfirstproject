50326d947a2904d3d81f86d7f2a41280d28700a7
1407763 <1407763@rgu.ac.uk>
2017-04-26 14:37:54 +0100

    users to photos

--- Coursework/edit.php
+++ Coursework/edit.php
@@ -23,9 +23,7 @@ $target_dir = 'uploads/'; // specifies the directory where the file is going to 
 if (isset($_GET['id'])){
      $id = $_GET['id'];
 
-    $id = trim($_POST['id']);
-    $id = mysqli_real_escape_string($link, $_POST ['id']);
-    $id  = htmlspecialchars(strip_tags($id));
+   
 
      $sql = "select * from tbl_images where id=" .$id; // finds the record of specific idea to be edited if cant find it displays error
      $result = mysqli_query($link, $sql);
