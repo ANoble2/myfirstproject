d1d63cb7723547f9f4cd40b2010bc022e6de3d10
ANoble2 <1407763@rgu.ac.uk>
2017-05-06 19:49:30 +0100

    comment box extras

---
+++ phpDemo/exam.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ashley
+ * Date: 06/05/2017
+ * Time: 19:36
+ */
+
+if ($_SERVER['request_method'] === 'get') {
+    ?>
+<form action="<?php echo "$_SERVER ['PHP_SELF']";?>" method="post">
+    <label>Forename</label>
+    <input type="text" name="forname"/>
+    <label>Surname</label>
+    <input type="text" name="surname"/>
+    <input type="submit" value="Go Go Go!"/>
+</form>
+<?php
+} elseif ($_SERVER['request_method'] === 'post') {
+
+    $forname = $_POST["forename"];
+    $surename= $_POST["surname"];
+    echo "Hello {$forname}{$surename}";
+}
+
+    ?>
